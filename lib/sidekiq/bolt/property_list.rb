e11c2911f7fa3644b36a53d8831900ce16692fc9
Thomas Rogers <thomasrogers03@gmail.com>
2015-10-23 08:40:53 -0400

    Implement PropertyList property value caching

--- lib/sidekiq/bolt/property_list.rb
+++ lib/sidekiq/bolt/property_list.rb
@@ -3,11 +3,22 @@ module Sidekiq
     module PropertyList
 
       def define_property(namespace, property)
+        instance_variable = :"@#{property}"
+
         define_method("#{property}=") do |value|
-          Bolt.redis { |conn| conn.set("#{namespace}:#{property}", value) }
+          Bolt.redis do |conn|
+            instance_variable_set(instance_variable, value)
+            conn.set("#{namespace}:#{property}", value)
+          end
         end
         define_method(property) do
-          Bolt.redis { |conn| conn.get("#{namespace}:#{property}") }
+          Bolt.redis do |conn|
+            result = instance_variable_get(instance_variable)
+            return result if result
+
+            result = conn.get("#{namespace}:#{property}")
+            instance_variable_set(instance_variable, result)
+          end
         end
       end
 
--- spec/classes/property_list_spec.rb
+++ spec/classes/property_list_spec.rb
@@ -34,15 +34,48 @@ describe Sidekiq::Bolt::PropertyList do
         expect(subject.type).to eq('L')
       end
 
+      it 'should cache the value' do
+        property_klass.define_property(:queue, :type)
+        global_redis.set('queue:type', 'L')
+        subject.type
+        expect_any_instance_of(Redis).not_to receive(:get)
+        subject.type
+      end
+
       context 'with a different property' do
         it 'should define a method to retrieve a property from redis' do
           property_klass.define_property(:meta_data, :project_name)
           global_redis.set('meta_data:project_name', 'The Do Everything Project')
           expect(subject.project_name).to eq('The Do Everything Project')
         end
+
+        it 'should cache the right value' do
+          property_klass.define_property(:queue, :type)
+          property_klass.define_property(:meta_data, :project_name)
+          global_redis.set('queue:type', 'L')
+          global_redis.set('meta_data:project_name', 'The Do Everything Project')
+          subject.type
+          expect(subject.project_name).to eq('The Do Everything Project')
+        end
       end
     end
 
+    it 'should cache the property when setting it' do
+      property_klass.define_property(:queue, :type)
+      subject.type = 'Q'
+      expect_any_instance_of(Redis).not_to receive(:get)
+      subject.type
+    end
+
+    context 'with a different property' do
+      it 'should cache the right value' do
+        property_klass.define_property(:queue, :type)
+        property_klass.define_property(:meta_data, :project_name)
+        subject.type = 'Q'
+        subject.project_name = 'Big Worker Man'
+        expect(subject.type).to eq('Q')
+      end
+    end
   end
 
 end
