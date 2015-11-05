f72651a5b6000196f7d7a9b3ed0b75a81a6ccad9
Thomas Rogers <thomasrogers03@gmail.com>
2015-11-05 07:42:05 -0500

    Bolt.redis now uses the Sidekiq redis pool

--- lib/sidekiq/bolt/redis_helpers.rb
+++ lib/sidekiq/bolt/redis_helpers.rb
@@ -1,15 +1,9 @@
 module Sidekiq
   module Bolt
 
-    def self.redis(&block)
-      @redis ||= begin
-        ConnectionPool.new do
-          conn = Redis.new(host: 'redis.dev', db: 13)
-          Redis::Namespace.new(:bolt, redis: conn)
-        end
-      end
-      @redis.with(&block)
-    end
+  def self.redis(&block)
+    Sidekiq.redis(&block)
+  end
 
   end
 end
--- spec/classes/redis_helpers_spec.rb
+++ spec/classes/redis_helpers_spec.rb
@@ -37,6 +37,16 @@ module Sidekiq
             expect(other_conn).not_to eq(conn)
           end
         end
+
+        context 'with a different redis connection configured through sidekiq' do
+          before { Sidekiq.redis = {url: 'redis://redis.dev/12', namespace: :bolty} }
+
+          it 'should yield the configured connection' do
+            Bolt.redis { |conn| conn.set('hello', 'world') }
+            result = Sidekiq.redis { |conn| conn.get('hello') }
+            expect(result).to eq('world')
+          end
+        end
       end
 
     end
