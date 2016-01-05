87150a0a004287690e3a1b489b88995368d58651
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-05 18:33:57 -0500

    Implement Queue.all

--- lib/sidekiq/bolt/queue.rb
+++ lib/sidekiq/bolt/queue.rb
@@ -2,6 +2,12 @@ module Sidekiq
   module Bolt
     class Queue < Struct.new(:name)
 
+      def self.all
+        Bolt.redis do |conn|
+          conn.smembers('queues')
+        end.map { |name| new(name) }
+      end
+
       def resources
         Bolt.redis do |conn|
           conn.smembers("queue:resources:#{name}")
--- spec/classes/queue_spec.rb
+++ spec/classes/queue_spec.rb
@@ -9,6 +9,26 @@ module Sidekiq
 
       subject { queue }
 
+      describe '.all' do
+        let(:resource) { Resource.new(Faker::Lorem.word) }
+
+        subject { Queue.all }
+
+        before { resource.add_work(name, '') }
+
+        it { is_expected.to eq([queue]) }
+
+        context 'with multiple queues' do
+          let(:resource_two) { Resource.new(Faker::Lorem.word) }
+          let(:name_two) { Faker::Lorem.word }
+          let(:queue_two) { Queue.new(name_two) }
+
+          before { resource_two.add_work(name_two, '') }
+
+          it { is_expected.to match_array([queue, queue_two]) }
+        end
+      end
+
       describe '#name' do
         its(:name) { is_expected.to eq(name) }
       end
