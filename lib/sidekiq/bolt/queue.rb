f977f2526b3649df198259e0ef7b499d7cdc193b
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-05 18:31:28 -0500

    Implement Queue#resources

--- lib/sidekiq/bolt/queue.rb
+++ lib/sidekiq/bolt/queue.rb
@@ -2,7 +2,11 @@ module Sidekiq
   module Bolt
     class Queue < Struct.new(:name)
 
-
+      def resources
+        Bolt.redis do |conn|
+          conn.smembers("queue:resources:#{name}")
+        end.map { |name| Resource.new(name) }
+      end
 
     end
   end
--- spec/classes/queue_spec.rb
+++ spec/classes/queue_spec.rb
@@ -13,6 +13,29 @@ module Sidekiq
         its(:name) { is_expected.to eq(name) }
       end
 
+      describe '#resources' do
+        let(:resource) { Resource.new(Faker::Lorem.word) }
+
+        before { resource.add_work(name, '') }
+
+        its(:resources) { is_expected.to eq([resource]) }
+
+        context 'with multiple resources' do
+          let(:resource_two) { Resource.new(Faker::Lorem.word) }
+          let(:name_two) { name }
+
+          before { resource_two.add_work(name_two, '') }
+
+          its(:resources) { is_expected.to match_array([resource, resource_two]) }
+
+          context 'when the resource does not associate with this queue' do
+            let(:name_two) { Faker::Lorem.word }
+
+            its(:resources) { is_expected.to eq([resource]) }
+          end
+        end
+      end
+
     end
   end
 end
