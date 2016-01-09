bc7dcb649f872b14b033fad582e550289d331cc8
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-09 15:19:49 -0500

    Sidekiq::Bolt.configure_server now adds the BlockQueue client middleware; implement .configure_client to do the same

--- lib/sidekiq/bolt/server_configuration.rb
+++ lib/sidekiq/bolt/server_configuration.rb
@@ -1,5 +1,6 @@
 module Sidekiq
   module Bolt
+
     def self.configure_server(config)
       config.options[:fetch] = Fetch
       config.server_middleware do |chain|
@@ -7,6 +8,16 @@ module Sidekiq
         chain.remove Middleware::Server::RetryJobs
         chain.add ServerMiddleware::RetryJobs
       end
+      config.client_middleware do |chain|
+        chain.add ClientMiddleware::BlockQueue
+      end
     end
+
+    def self.configure_client(config)
+      config.client_middleware do |chain|
+        chain.add ClientMiddleware::BlockQueue
+      end
+    end
+
   end
 end
--- spec/classes/server_configuration_spec.rb
+++ spec/classes/server_configuration_spec.rb
@@ -11,16 +11,28 @@ module Sidekiq
           expect(Sidekiq.options[:fetch]).to eq(Fetch)
         end
 
-        it 'should remove the Sidekiq::Middleware::Server::RetryJobs' do
+        it 'should remove the Sidekiq::Middleware::Server::RetryJobs server middleware' do
           expect(Sidekiq.server_middleware).not_to exist(Middleware::Server::RetryJobs)
         end
 
-        it 'should add the Sidekiq::Bolt::ServerMiddleware::RetryJobs' do
+        it 'should add the Sidekiq::Bolt::ServerMiddleware::RetryJobs server middleware' do
           expect(Sidekiq.server_middleware).to exist(ServerMiddleware::RetryJobs)
         end
 
-        it 'should add the Sidekiq::Bolt::ServerMiddleware::MetaDataMiddleware as the first middleware' do
+        it 'should add the Sidekiq::Bolt::ServerMiddleware::MetaDataMiddleware as the first server middleware' do
           expect(Sidekiq.server_middleware.first.klass).to eq(ServerMiddleware::JobMetaData)
+        end
+
+        it 'should add the Sidekiq::Bolt::ClientMiddleware::BlockQueue client middleware' do
+          expect(Sidekiq.client_middleware).to exist(ClientMiddleware::BlockQueue)
+        end
+      end
+
+      describe '.configure_client' do
+        before { Bolt.configure_client(Sidekiq) }
+
+        it 'should add the Sidekiq::Bolt::ClientMiddleware::BlockQueue client middleware' do
+          expect(Sidekiq.client_middleware).to exist(ClientMiddleware::BlockQueue)
         end
       end
 
