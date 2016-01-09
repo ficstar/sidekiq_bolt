f39ba375ea6c81201edfd3865169c9bbb86024e5
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-09 15:50:52 -0500

    RetryJobs middleware now uses a worker callback to determine if we should retry a job

--- lib/sidekiq/bolt/server_middleware/retry_jobs.rb
+++ lib/sidekiq/bolt/server_middleware/retry_jobs.rb
@@ -6,7 +6,7 @@ module Sidekiq
         def call(worker, job, queue)
           yield
         rescue Exception => e
-          raise unless job['retry']
+          raise unless job['retry'] && (!worker.sidekiq_should_retry_block || worker.sidekiq_should_retry_block.call(job, e))
 
           job['error'] = e
           serialized_job = Sidekiq.dump_json(job)
--- spec/classes/server_middleware/retry_jobs_spec.rb
+++ spec/classes/server_middleware/retry_jobs_spec.rb
@@ -5,6 +5,13 @@ module Sidekiq
     module ServerMiddleware
       describe RetryJobs do
 
+        let(:worker_class) do
+          Class.new do
+            include Worker
+          end
+        end
+        let(:worker) { worker_class.new }
+
         describe '#call' do
           let(:queue_name) { Faker::Lorem.word }
           let(:resource_name) { Faker::Lorem.word }
@@ -12,7 +19,7 @@ module Sidekiq
           let(:job) { {'queue' => queue_name, 'resource' => resource_name, 'retry' => retry_job} }
 
           it 'should yield' do
-            expect { |block| subject.call(nil, job, nil, &block) }.to yield_control
+            expect { |block| subject.call(worker, job, nil, &block) }.to yield_control
           end
 
           context 'when the block raises an error' do
@@ -23,22 +30,22 @@ module Sidekiq
             let(:error_job) { Sidekiq.load_json(error_work[1]) }
 
             it 'should count this work as retrying' do
-              subject.call(nil, job, nil) { raise error }
+              subject.call(worker, job, nil) { raise error }
               expect(resource.retrying).to eq(1)
             end
 
             it 'should add this work back to the resource' do
-              subject.call(nil, job, nil) { raise error }
+              subject.call(worker, job, nil) { raise error }
               expect(error_job).to include(job)
             end
 
             it 'should add it for the original queue' do
-              subject.call(nil, job, nil) { raise error }
+              subject.call(worker, job, nil) { raise error }
               expect(error_queue).to eq(queue_name)
             end
 
             it 'should include the error information in the retrying job' do
-              subject.call(nil, job, nil) { raise error }
+              subject.call(worker, job, nil) { raise error }
               expect(error_job['error']).to eq(error)
             end
 
@@ -46,7 +53,36 @@ module Sidekiq
               let(:retry_job) { false }
 
               it 'should raise the error' do
-                expect { subject.call(nil, job, nil) { raise error } }.to raise_error(error)
+                expect { subject.call(worker, job, nil) { raise error } }.to raise_error(error)
+              end
+            end
+
+            context 'when the worker specifies a sidekiq_should_retry_block' do
+              let(:worker_class) do
+                Class.new do
+                  include Worker
+                  should_retry? { |job, error| !error.is_a?(StandardError) && !job['borked!'] }
+                end
+              end
+
+              it 'should use that block to determine whether or not to retry' do
+                expect { subject.call(worker, job, nil) { raise error } }.to raise_error(error)
+              end
+
+              context 'with a different error' do
+                let(:error) { Interrupt.new }
+
+                it 'should use that block to determine whether or not to retry' do
+                  expect { subject.call(worker, job, nil) { raise error } }.not_to raise_error
+                end
+              end
+
+              context 'when the job contains something that the worker does not like' do
+                before { job['borked!'] = true }
+
+                it 'should not retry' do
+                  expect { subject.call(worker, job, nil) { raise error } }.to raise_error(error)
+                end
               end
             end
           end
