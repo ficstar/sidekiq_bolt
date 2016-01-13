50349c56ab76b6a9f8a27e42ccaa84c50f85f4a0
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-13 08:30:44 -0500

    Add support for counter properties

--- lib/sidekiq/bolt/property_list.rb
+++ lib/sidekiq/bolt/property_list.rb
@@ -11,13 +11,35 @@ module Sidekiq
             conn.set("#{namespace}:#{name}", value)
           end
         end
+        if type == :counter
+          define_method("#{property}_incr") do
+            Bolt.redis do |conn|
+              conn.incr("#{namespace}:#{name}")
+            end
+          end
+          define_method("#{property}_incrby") do |amount|
+            Bolt.redis do |conn|
+              conn.incrby("#{namespace}:#{name}", amount)
+            end
+          end
+          define_method("#{property}_decr") do
+            Bolt.redis do |conn|
+              conn.decr("#{namespace}:#{name}")
+            end
+          end
+          define_method("#{property}_decrby") do |amount|
+            Bolt.redis do |conn|
+              conn.decrby("#{namespace}:#{name}", amount)
+            end
+          end
+        end
         define_method(property) do
           Bolt.redis do |conn|
             result = instance_variable_get(instance_variable)
             return result if result
 
             result = conn.get("#{namespace}:#{name}")
-            result = result.to_i if type == :int
+            result = result.to_i if [:int, :counter].include?(type)
             instance_variable_set(instance_variable, result)
           end
         end
--- spec/classes/property_list_spec.rb
+++ spec/classes/property_list_spec.rb
@@ -95,6 +95,56 @@ describe Sidekiq::Bolt::PropertyList do
         expect(property_klass.new(name).busy).to eq(5)
       end
     end
+
+    context 'with a counter property' do
+      let(:property) { :busy }
+
+      before { property_klass.define_property(:queue_busy, property, :counter) }
+
+      it 'should should define a method to increment the counter' do
+        subject.busy_incr
+        expect(property_klass.new(name).busy).to eq(1)
+      end
+
+      it 'should should define a method to increment the counter by a specified number' do
+        subject.busy_incrby(5)
+        expect(property_klass.new(name).busy).to eq(5)
+      end
+
+      it 'should should define a method to decrement the counter' do
+        subject.busy_decr
+        expect(property_klass.new(name).busy).to eq(-1)
+      end
+
+      it 'should should define a method to decrement the counter by a specified number' do
+        subject.busy_decrby(6)
+        expect(property_klass.new(name).busy).to eq(-6)
+      end
+
+      context 'with a different property' do
+        let(:property) { :errors }
+
+        it 'should should define a method to increment the counter' do
+          subject.errors_incr
+          expect(property_klass.new(name).errors).to eq(1)
+        end
+
+        it 'should should define a method to increment the counter by a specified number' do
+          subject.errors_incrby(15)
+          expect(property_klass.new(name).errors).to eq(15)
+        end
+
+        it 'should should define a method to decrement the counter' do
+          subject.errors_decr
+          expect(property_klass.new(name).errors).to eq(-1)
+        end
+
+        it 'should should define a method to decrement the counter by a specified number' do
+          subject.errors_decrby(9)
+          expect(property_klass.new(name).errors).to eq(-9)
+        end
+      end
+    end
   end
 
 end
