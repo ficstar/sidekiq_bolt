30061e10f4e563d780582d767474b615c4b33bc6
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-19 08:12:12 -0500

    Implement .peform_async and .perform_async_with_options for FutureWorker

--- lib/sidekiq/bolt/future_worker.rb
+++ lib/sidekiq/bolt/future_worker.rb
@@ -4,6 +4,22 @@ module Sidekiq
 
       def self.included(base)
         base.send(:include, Worker)
+        base.extend ClassMethods
+      end
+
+      module ClassMethods
+        def perform_async(*args, &block)
+          client_push({'class' => self, 'args' => args, 'resource' => '$async_local'}, &block)
+        end
+
+        def perform_async_with_options(options, *args, &block)
+          #noinspection RubySuperCallWithoutSuperclassInspection
+          super(options.merge(resource: '$async_local'), *args, &block)
+        end
+
+        def perform_in(*_)
+          raise NotImplementedError, '.peform_in not implemented for FutureWorkers'
+        end
       end
 
       def perform(*args)
--- spec/classes/future_worker_spec.rb
+++ spec/classes/future_worker_spec.rb
@@ -10,21 +10,54 @@ module Sidekiq
         end
       end
 
-      let(:worker_class_base) do
-        Class.new do
+      module FutureWorker
+        class MockWorker
           include FutureWorker
-          define_method(:perform_future) { |*args| MockFuture.new }
+          sidekiq_options resource: Faker::Lorem.word
+          define_method(:perform_future) { |*_| MockFuture.new }
+        end
+
+        class MockErrorWorker
+          include FutureWorker
+          sidekiq_options resource: Faker::Lorem.word
+          define_method(:perform_future) { |*args| MockFuture.new(StandardError.new(args.inspect)) }
         end
       end
-      let(:worker_class) { worker_class_base }
+
+      let(:worker_class) { FutureWorker::MockWorker }
       let(:worker) { worker_class.new }
       let(:args) { Faker::Lorem.paragraphs }
+      let(:resource) { Resource.new('$async_local') }
 
       subject { worker }
 
-      before { allow(worker).to receive(:acknowledge_work) }
+      before do
+        Bolt.configure_server(Sidekiq)
+        #Sidekiq.server_middleware { |chain| chain.remove(Middleware::Server::RetryJobs) }
+        allow(worker).to receive(:acknowledge_work)
+      end
 
       it { is_expected.to be_a_kind_of(Worker) }
+
+      describe '.perform_async' do
+        it 'should run the work locally regardless of sidekiq options' do
+          expect_any_instance_of(worker_class).to receive(:perform).with(*args)
+          worker_class.perform_async(*args)
+        end
+      end
+
+      describe '.perform_async_with_options' do
+        it 'should run the work locally regardless of sidekiq options' do
+          expect_any_instance_of(worker_class).to receive(:perform).with(*args)
+          worker_class.perform_async_with_options({resource: Faker::Lorem.word}, *args)
+        end
+      end
+
+      describe '.perform_in' do
+        it 'should raise an error indicating that it is not supported' do
+          expect { worker_class.perform_in(120, *args) }.to raise_error(NotImplementedError, '.peform_in not implemented for FutureWorkers')
+        end
+      end
 
       describe '#perform' do
         it 'should acknowledge the work' do
@@ -33,11 +66,7 @@ module Sidekiq
         end
 
         context 'when the future returns an error' do
-          let(:worker_class) do
-            Class.new(worker_class_base) do
-              define_method(:perform_future) { |*args| MockFuture.new(StandardError.new(args.inspect)) }
-            end
-          end
+          let(:worker_class) { FutureWorker::MockErrorWorker }
 
           it 'should acknowledge the work with the error' do
             expect(worker).to receive(:acknowledge_work) do |error|
