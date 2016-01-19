161efd62f5e2c827b48b9b7cac4abac66b123013
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-18 21:14:07 -0500

    JobMetaData middleware now sets the worker's jid, regardless if it has been set already

--- lib/sidekiq/bolt/server_middleware/job_meta_data.rb
+++ lib/sidekiq/bolt/server_middleware/job_meta_data.rb
@@ -6,6 +6,7 @@ module Sidekiq
         def call(worker, job, _)
           worker.queue = Queue.new(job['queue'])
           worker.resource = Resource.new(job['resource'])
+          worker.jid = job['jid']
           worker.parent_job_id = job['pjid']
           worker.original_message = Sidekiq.dump_json(job)
           yield
--- spec/classes/server_middleware/job_meta_data_spec.rb
+++ spec/classes/server_middleware/job_meta_data_spec.rb
@@ -6,12 +6,13 @@ module Sidekiq
       describe JobMetaData do
 
         describe '#call' do
-          let(:worker) { Struct.new(:queue, :resource, :parent_job_id, :original_message).new }
+          let(:worker) { Struct.new(:queue, :resource, :jid, :parent_job_id, :original_message).new }
           let(:queue_name) { Faker::Lorem.word }
           let(:resource_name) { Faker::Lorem.word }
+          let(:job_id) { SecureRandom.uuid }
           let(:parent_job_id) { SecureRandom.uuid }
           #noinspection RubyStringKeysInHashInspection
-          let(:job) { {'queue' => queue_name, 'resource' => resource_name, 'pjid' => parent_job_id} }
+          let(:job) { {'queue' => queue_name, 'resource' => resource_name, 'jid' => job_id, 'pjid' => parent_job_id} }
           let(:original_message) { Sidekiq.load_json(subject.original_message) }
 
           it 'should yield' do
@@ -26,6 +27,7 @@ module Sidekiq
             its(:queue) { is_expected.to eq(Queue.new(queue_name)) }
             its(:resource) { is_expected.to eq(Resource.new(resource_name)) }
             its(:parent_job_id) { is_expected.to eq(parent_job_id) }
+            its(:jid) { is_expected.to eq(job_id) }
             it { expect(original_message).to eq(job) }
           end
 
