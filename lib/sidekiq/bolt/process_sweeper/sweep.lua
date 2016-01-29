4c2a118ff66f3179b586e1767fce17c3c61cac48
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-29 18:12:57 -0500

    Implement ProcessSweeper#sweep to recover work lost from a dead sidekiq process

--- lib/sidekiq/bolt/process_sweeper/sweep.lua
+++ lib/sidekiq/bolt/process_sweeper/sweep.lua
@@ -1,5 +1,21 @@
 local namespace = table.remove(KEYS, 1)
 local process = table.remove(ARGV, 1)
 local processes_set_key = namespace .. 'bolt:processes'
+local worker_backup_key = namespace .. 'resource:backup:worker:' .. process
+
+local processing_work = redis.call('lrange', worker_backup_key, 0, -1)
+
+for _, serialized_work in ipairs(processing_work) do
+    local work = cjson.decode(serialized_work)
+
+    local resource_allocated_key = namespace .. 'resource:allocated:' .. work.resource
+    local queue_key = namespace .. 'resource:queue:retrying:' .. work.queue .. ':' .. work.resource
+    local queue_busy_key = namespace .. 'queue:busy:' .. work.queue
+
+    redis.call('lpush', queue_key, work.work)
+    redis.call('decr', resource_allocated_key)
+    redis.call('decr', queue_busy_key)
+end
+redis.call('del', worker_backup_key)
 
 redis.call('srem', processes_set_key, process)
--- spec/classes/process_sweeper_spec.rb
+++ spec/classes/process_sweeper_spec.rb
@@ -12,17 +12,20 @@ module Sidekiq
 
       describe '#sweep' do
         let(:queue_name) { Faker::Lorem.word }
+        let(:queue) { Queue.new(queue_name) }
         let(:resource_name) { Faker::Lorem.word }
+        let(:resource) { Resource.new(resource_name) }
         let(:work) { SecureRandom.uuid }
         let(:backup_work) { {'queue' => queue_name, 'resource' => resource_name, 'work' => work, } }
         let(:serialized_backup_work) { JSON.dump(backup_work) }
-        let(:resource) { Resource.new(resource_name) }
         let(:result_allocation) { resource.allocate(1) }
         let(:result_queue) { result_allocation[0] }
         let(:result_work) { result_allocation[1] }
 
         before do
           global_redis.lpush("resource:backup:worker:#{process}", serialized_backup_work)
+          resource.add_work(queue_name, work)
+          resource.allocate(1)
         end
 
         it 'should remove the process from the processes set' do
@@ -30,14 +33,57 @@ module Sidekiq
           expect(global_redis.smembers('bolt:processes')).not_to include(process)
         end
 
-        xit 'should re-insert the work back into the queue' do
+        it 'should re-insert the work back into the queue' do
           subject.sweep
           expect(result_work).to eq(work)
         end
 
-        xit 'should re-insert into the right queue' do
+        it 'should re-insert into the right queue' do
           subject.sweep
           expect(result_queue).to eq(queue_name)
+        end
+
+        context 'with more backed up work' do
+          let(:queue_name_two) { Faker::Lorem.word }
+          let(:backup_work_two) { {'queue' => queue_name_two, 'resource' => resource_name, 'work' => work, } }
+          let(:serialized_backup_work_two) { JSON.dump(backup_work_two) }
+          let(:result_allocation_two) { resource.allocate(1) }
+          let(:result_queue_two) { result_allocation[0] }
+          let(:result_work_two) { result_allocation[1] }
+
+          before do
+            global_redis.lpush("resource:backup:worker:#{process}", serialized_backup_work_two)
+          end
+
+          it 'should re-insert the work back into the queue' do
+            subject.sweep
+            expect(result_work).to eq(work)
+          end
+
+          it 'should re-insert into the right queue' do
+            subject.sweep
+            expect(result_queue).to eq(queue_name)
+          end
+
+          it 'should remove all work from the queue' do
+            subject.sweep
+            expect(global_redis.lrange("resource:backup:worker:#{process}", 0, -1)).to be_empty
+          end
+        end
+
+        it 'should count the work as retrying' do
+          subject.sweep
+          expect(resource.retrying).to eq(1)
+        end
+
+        it 'should decrement the resource allocation count' do
+          subject.sweep
+          expect(resource.allocated).to eq(0)
+        end
+
+        it 'should decrement the queue busy count' do
+          subject.sweep
+          expect(queue.busy).to eq(0)
         end
       end
 
