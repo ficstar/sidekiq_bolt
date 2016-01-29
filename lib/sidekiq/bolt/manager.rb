06dd88c23f7d2ac5f384dc08e16c4657fecefad9
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-29 08:37:50 -0500

    Refactor Heartbeat behaviour into module so that it can be re-used/moved easily

--- lib/sidekiq/bolt/manager.rb
+++ lib/sidekiq/bolt/manager.rb
@@ -2,17 +2,24 @@
 require 'sidekiq/manager'
 
 module Sidekiq
-  class Manager
-    def bolt_heartbeat(identity, serialized_info)
-      Bolt.redis do |conn|
-        conn.pipelined do
-          conn.sadd('bolt:processes', identity)
-          conn.set("bolt:processes:#{identity}", serialized_info)
-          conn.expire("bolt:processes:#{identity}", 60)
+  module Bolt
+    module Heartbeat
+
+      def bolt_heartbeat(identity, serialized_info)
+        Bolt.redis do |conn|
+          conn.pipelined do
+            conn.sadd('bolt:processes', identity)
+            conn.set("bolt:processes:#{identity}", serialized_info)
+            conn.expire("bolt:processes:#{identity}", 60)
+          end
         end
       end
-    end
 
+    end
+  end
+
+  class Manager
+    include Bolt::Heartbeat
     alias :'❤' :bolt_heartbeat
   end
 end
