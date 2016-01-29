f8b19a6f30658dfbdd374cc7816bcf01dc375aa7
Thomas Rogers <thomasrogers03@gmail.com>
2016-01-29 18:48:56 -0500

    Add some debug logging

--- lib/sidekiq/bolt/job_recovery_enq.rb
+++ lib/sidekiq/bolt/job_recovery_enq.rb
@@ -4,13 +4,17 @@ module Sidekiq
 
       def enqueue_jobs
         processes = Bolt.redis { |redis| redis.smembers('bolt:processes') }
+        Sidekiq.logger.debug "JobRecoveryEnq pulled process list: #{processes.inspect}"
         process_states = Bolt.redis do |redis|
           redis.multi do
             processes.each { |process| redis.exists("bolt:processes:#{process}") }
           end
         end
         processes.each.with_index do |process, index|
-          ProcessSweeper.new(process).sweep unless process_states[index]
+          unless process_states[index]
+            Sidekiq.logger.debug "process '#{process}' has died, recovering its work"
+            ProcessSweeper.new(process).sweep
+          end
         end
       end
 
