6f288f88c88c3d3f3b841af2dcc68cdd74fd2c9a
Thomas Rogers <thomasrogers03@gmail.com>
2016-04-05 15:26:20 -0400

    Refactor WorkerContext

--- lib/sidekiq/bolt/server_middleware/worker_context.rb
+++ lib/sidekiq/bolt/server_middleware/worker_context.rb
@@ -3,30 +3,46 @@ module Sidekiq
     module ServerMiddleware
       class WorkerContext
 
-        def call(worker, job, _)
+        def call(worker, job, _, &block)
           successfully_setup = worker.respond_to?(:setup) ? worker.setup : true
 
           if successfully_setup
-            if worker.respond_to?(:context)
-              valid_context = false
-              worker.context do
-                valid_context = true
-                yield
-              end
-              raise "Expected worker '#{worker.class}' #context to yield, but it didn't!" unless valid_context
-            else
-              yield
-            end
+            handle_successful_setup(worker, &block)
           else
-            Sidekiq.logger.debug 'Worker skipped due to #setup returning false'
-
-            serialized_job = Sidekiq.dump_json(job)
-            worker.resource.add_work(job['queue'], serialized_job)
-            job.delete('jid')
+            handle_erroneous_setup(job, worker)
           end
 
         ensure
           worker.teardown if worker.respond_to?(:teardown)
+        end
+
+        private
+
+        def handle_successful_setup(worker, &block)
+          if worker.respond_to?(:context)
+            unless yield_worker_context(worker, &block)
+              raise "Expected worker '#{worker.class}' #context to yield, but it didn't!"
+            end
+          else
+            yield
+          end
+        end
+
+        def yield_worker_context(worker)
+          valid_context = false
+          worker.context do
+            valid_context = true
+            yield
+          end
+          valid_context
+        end
+
+        def handle_erroneous_setup(job, worker)
+          Sidekiq.logger.debug 'Worker skipped due to #setup returning false'
+
+          serialized_job = Sidekiq.dump_json(job)
+          worker.resource.add_work(job['queue'], serialized_job)
+          job.delete('jid')
         end
 
       end
