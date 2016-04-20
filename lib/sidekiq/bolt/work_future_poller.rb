1129c73e04c091491cd3397d4ae63f3189c9086d
Thomas Rogers <thomasrogers03@gmail.com>
2016-04-19 21:07:10 -0400

    Refactor WorkFuturePoller for performance

--- lib/sidekiq/bolt/work_future_poller.rb
+++ lib/sidekiq/bolt/work_future_poller.rb
@@ -10,8 +10,16 @@ module Sidekiq
       end
 
       def enqueue_jobs
-        WAITING.keys.each do |job_id|
-          serialized_result = Bolt.redis { |redis| redis.get("worker:results:#{job_id}") }
+        pending_jobs = WAITING.keys
+        serialized_results = Bolt.redis do |redis|
+          redis.multi do
+            pending_jobs.each do |job_id|
+              redis.get("worker:results:#{job_id}")
+            end
+          end
+        end
+        serialized_results.each.with_index do |serialized_result, index|
+          job_id = pending_jobs[index]
           if serialized_result
             result = Sidekiq.load_json(serialized_result)
             observable = WAITING[job_id]
