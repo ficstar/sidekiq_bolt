d08f20803e43438dd059439fc2256df201634cbf
Thomas Rogers <thomasrogers03@gmail.com>
2016-04-19 21:08:33 -0400

    Refactor WorkFuturePoller

--- lib/sidekiq/bolt/work_future_poller.rb
+++ lib/sidekiq/bolt/work_future_poller.rb
@@ -11,20 +11,29 @@ module Sidekiq
 
       def enqueue_jobs
         pending_jobs = WAITING.keys
-        serialized_results = Bolt.redis do |redis|
+        serialized_results(pending_jobs).each.with_index do |serialized_result, index|
+          job_id = pending_jobs[index]
+          resolve_future(job_id, serialized_result)
+        end
+      end
+
+      private
+
+      def resolve_future(job_id, serialized_result)
+        if serialized_result
+          result = Sidekiq.load_json(serialized_result)
+          observable = WAITING[job_id]
+          observable.set(result)
+          WAITING.delete(job_id)
+        end
+      end
+
+      def serialized_results(pending_jobs)
+        Bolt.redis do |redis|
           redis.multi do
             pending_jobs.each do |job_id|
               redis.get("worker:results:#{job_id}")
             end
-          end
-        end
-        serialized_results.each.with_index do |serialized_result, index|
-          job_id = pending_jobs[index]
-          if serialized_result
-            result = Sidekiq.load_json(serialized_result)
-            observable = WAITING[job_id]
-            observable.set(result)
-            WAITING.delete(job_id)
           end
         end
       end
