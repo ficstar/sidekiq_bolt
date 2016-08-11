ec5fbd5b13e2aa4ebd5b5a7ee89f74a2108aaf4a
Thomas Rogers <thomasrogers03@gmail.com>
2016-08-11 07:38:57 -0400

    Minor refactor

--- lib/sidekiq/bolt/processor.rb
+++ lib/sidekiq/bolt/processor.rb
@@ -7,7 +7,7 @@ module Sidekiq
       worker_klass = msg['class'].constantize
       worker = worker_klass.new
       ack = false
-      future = ThomasUtils::Future.value(nil).then do
+      future = ThomasUtils::Future.none.then do
         Sidekiq.server_middleware.invoke(worker, msg, work.queue_name) do
           ack = true
           execute_job(worker, cloned(msg['args']))
