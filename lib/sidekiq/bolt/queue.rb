694598162bdf279924a83620e638e2b01d8e567f
Thomas Rogers <thomasrogers03@gmail.com>
2016-08-17 08:30:28 -0400

    Refactor Queue.all

--- lib/sidekiq/bolt/queue.rb
+++ lib/sidekiq/bolt/queue.rb
@@ -14,24 +14,30 @@ module Sidekiq
 
       define_property 'queue:group', :group
 
-      def self.all(group = :*)
-        if group == :*
-          Bolt.redis do |conn|
-            conn.smembers('queues')
-          end
-        else
-          run_script(:grouped_queues, FILTER_GROUPED_QUEUES_SCRIPT, NAMESPACE_KEY, [group])
-        end.map { |name| new(name) }
-      end
+      class << self
+        def all(group = :*)
+          grouped_all(group).map { |name| new(name) }
+        end
 
-      def self.enqueue(list_of_items)
-        script_sha = Scripts.load_script(:resource_add_work, Resource::ADD_WORK_SCRIPT)
-        Bolt.redis do |redis|
-          redis.pipelined do
-            list_of_items.each do |item|
-              argv = [item[:queue], item[:resource], item[:work]]
-              redis.evalsha(script_sha, keys: NAMESPACE_KEY, argv: argv)
+        def enqueue(list_of_items)
+          script_sha = Scripts.load_script(:resource_add_work, Resource::ADD_WORK_SCRIPT)
+          Bolt.redis do |redis|
+            redis.pipelined do
+              list_of_items.each do |item|
+                argv = [item[:queue], item[:resource], item[:work]]
+                redis.evalsha(script_sha, keys: NAMESPACE_KEY, argv: argv)
+              end
             end
+          end
+        end
+
+        private
+
+        def grouped_all(group)
+          if group == :*
+            Bolt.redis { |conn| conn.smembers('queues') }
+          else
+            run_script(:grouped_queues, FILTER_GROUPED_QUEUES_SCRIPT, NAMESPACE_KEY, [group])
           end
         end
       end
