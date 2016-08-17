00a6fd2a59458205f0e8eb16ed37cb48ac1c9754
Thomas Rogers <thomasrogers03@gmail.com>
2016-08-17 17:41:54 -0400

    Implement a more reduced #marshal_dump for the Message hash

--- lib/sidekiq/bolt/message.rb
+++ lib/sidekiq/bolt/message.rb
@@ -1,6 +1,15 @@
 module Sidekiq
   module Bolt
     class Message < Hash
+      VALID_ATTRIBUTES = %w(class args queue resource jid pjid persist).freeze
+
+      def marshal_dump
+        values_at(*VALID_ATTRIBUTES)
+      end
+
+      def marshal_load(array)
+        VALID_ATTRIBUTES.each.with_index { |key, index| self[key] = array[index] }
+      end
     end
   end
 end
--- spec/classes/message_spec.rb
+++ spec/classes/message_spec.rb
@@ -4,7 +4,29 @@ module Sidekiq
   module Bolt
     describe Message do
 
+      let(:valid_attributes) { %w(class args queue resource jid pjid persist) }
+      let(:invalid_attributes) { Faker::Lorem.words }
+      let(:message_attributes) do
+        (valid_attributes + invalid_attributes).inject({}) { |memo, key| memo.merge!(key => Faker::Lorem.word) }
+      end
+      let(:message) { Message[message_attributes] }
+
+      subject { message }
+
       it { is_expected.to be_a_kind_of(Hash) }
+
+      describe '#marshal_dump' do
+        subject { message.marshal_dump }
+        it { is_expected.to eq(message_attributes.values_at(*valid_attributes)) }
+      end
+
+      describe '#marshal_load' do
+        subject { Message.new }
+
+        before { subject.marshal_load(message.marshal_dump) }
+
+        it { is_expected.to eq(message_attributes.slice(*valid_attributes)) }
+      end
 
     end
   end
